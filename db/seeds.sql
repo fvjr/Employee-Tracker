6882f65db7d4139b52556f35ab1378d933ad4566
Fernando Vasquez <vasquezjrf@gmail.com>
2022-02-21 18:38:32 -0500

    made initial schema and server files

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+-- INSERT INTO movies (movie_name)
+-- VALUES ("Blade"),
+--        ("Training Day"),
+--        ("Spider Man"),
+--        ("Iron Man"),
+--        ("Thor"),
+--        ("BatMan"),
+--        ("Day Man"),
+--        ("Night Man");
