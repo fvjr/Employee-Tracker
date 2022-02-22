e5ff768b977d05069e2a6441715edfaac3092f39
Fernando Vasquez <vasquezjrf@gmail.com>
2022-02-21 19:40:18 -0500

    made seed file for testing purposes

--- db/schema.sql
+++ db/schema.sql
@@ -1,36 +1,32 @@
 DROP DATABASE IF EXISTS employees_db;
-CREATE DATABASE eployees_db;
+CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE department(
-  id INT NOT NULL ,
-  name VARCHAR(30) NOT NUll, 
-  PRIMARY KEY (id)
+CREATE TABLE department (
+  id INT PRIMARY KEY,
+  name VARCHAR(30) 
 );
 
 CREATE TABLE role(
-  id INT NOT NULL,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INT NOT NULL,
-  PRIMARY KEY (id),
+  id INT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
   FOREIGN KEY (department_id)
   REFERENCES department(id)
-  ON DELETE SET NULL
 );
 
 CREATE TABLE employee(
-  id INT NOT NULL,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
+  id INT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
   manager_id INT,
-  PRIMARY KEY (id),
   FOREIGN KEY (role_id)
-  REFERENCES role(id)
-  ON DELETE SET NULL,
+  REFERENCES role(id),
+  -- //use a join
   FOREIGN KEY (manager_id)
-  REFERENCES employee(id),
+  REFERENCES employee(id)
   ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,35 @@
--- INSERT INTO movies (movie_name)
--- VALUES ("Blade"),
---        ("Training Day"),
---        ("Spider Man"),
---        ("Iron Man"),
---        ("Thor"),
---        ("BatMan"),
---        ("Day Man"),
---        ("Night Man");
+-- INSERT INTO department (id, name)
+-- VALUES (1, "Finance"),
+--        (2, "Legal"),
+--        (3, "Management"),
+--        (4, "Security");
+
+-- INSERT INTO role (id, title, salary, department_id)
+-- VALUES (111, "Director", 200000, 3),
+--        (222, "Lawyer", 100000, 2),
+--        (333, "Accountant", 83000, 1),
+--        (444, "Security", 72000, 4);
+
+-- INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+-- VALUES (1, "Fern", "V", 1, 0)
+--         (2, "Kyle", "S", 2, 0)
+--         (3, "Alec", "H", 3, 0)
+--         (4, "Nano", "S", 4, 0)
+
+INSERT INTO department (id, name)
+VALUES (1, "Finance"),
+       (2, "Legal"),
+       (3, "Management"),
+       (4, "Security");
+
+INSERT INTO role (id, title, salary)
+VALUES (66, "Director", 200000),
+       (677, "Lawyer", 100000),
+       (7, "Accountant", 83000),
+       (8, "Security", 72000);
+
+INSERT INTO employee (id, first_name, last_name)
+VALUES (9, "Fern", "V"),
+        (99, "Kyle", "S"),
+        (999, "Alec", "H"),
+        (9999, "Nano", "S");
