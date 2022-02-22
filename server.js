ea8ba7ce94efa6a45626f6aff9bb44b94f40fb59
Fernando Vasquez <vasquezjrf@gmail.com>
2022-02-21 20:40:55 -0500

    started logic for db queries

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
 const mysql = require('mysql2');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-const connection = mysql.createConnection({
+//Express middleware
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
+
+const db = mysql.createConnection({
 host: 'localhost',
 user: 'root',
 password: '',
@@ -14,7 +20,7 @@ console.log('Connected to employees_db database.')
 const mainMenuPrompts = [
   {
     type: 'list',
-    name: 'menu',
+    name: 'mainMenuPrompt',
     message: "What would you like to do next?",
     choices: [
       `View all departments`,
@@ -31,8 +37,29 @@ const mainMenuPrompts = [
 const mainMenuDisplay = () => {
   inquirer.prompt(mainMenuPrompts)
   .then((userChoice) => {
-    console.log(userChoice)
+    console.log(userChoice.mainMenuPrompt)
+    if (userChoice.mainMenuPrompt === `View all departments`) {
+      db.query('SELECT * FROM department', function (err, results)
+      {
+        console.log(results);
+      })
+    }
   })
 }
 
 mainMenuDisplay();
+
+//testing
+// db.query('SELECT * FROM role', function (err, results)
+// {
+//   console.log(results);
+// })
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+});
+
