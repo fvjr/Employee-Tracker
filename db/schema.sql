7b93e888864bcdd4efe531f5ef50feb5715eaa03
Fernando Vasquez <vasquezjrf@gmail.com>
2022-02-23 21:23:46 -0500

    developed logic for creation of roles and departments

--- db/schema.sql
+++ db/schema.sql
@@ -4,12 +4,12 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-  id INT PRIMARY KEY,
-  name VARCHAR(30) 
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
 );
 
 CREATE TABLE role(
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(30),
   salary DECIMAL,
   department_id INT,
--- server.js
+++ server.js
@@ -6,16 +6,16 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 //Express middleware
-app.use(express.urlencoded({ extended:true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const db = mysql.createConnection({
-host: 'localhost',
-user: 'root',
-password: '',
-database: 'employees_db'
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'employees_db'
 },
-console.log('Connected to employees_db database.')
+  console.log('Connected to employees_db database.')
 );
 
 const mainMenuPrompts = [
@@ -35,34 +35,83 @@ const mainMenuPrompts = [
   },
 ]
 
+const departmentPrompts = [
+  {
+    type: 'input',
+    name: 'departmentName',
+    message: "What is the name of the new department?",
+  }
+]
+
+const rolePrompts = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "What is the name of the new role?",
+  },
+  {
+    type: 'number',
+    name: 'salary',
+    message: "What is the salary for this role?"
+  },
+  {
+    type: 'input',
+    name: 'department',
+    message: 'What department does the role belong to?',
+    // choices: [`${db.query('SELECT * FROM department')}`]
+
+  }
+]
 const mainMenuDisplay = () => {
   inquirer.prompt(mainMenuPrompts)
-  .then((userChoice) => {
-    console.log(userChoice.mainMenuPrompt)
-    if (userChoice.mainMenuPrompt === `View all departments`) {
-      db.query('SELECT * FROM department', function (err, results)
-      {
-        console.table(results);
-        mainMenuDisplay();
+    .then((userChoice) => {
+      console.log(userChoice.mainMenuPrompt)
+      if (userChoice.mainMenuPrompt === `View all departments`) {
+        db.query('SELECT * FROM department', function (err, results) {
+          console.table(results);
+          mainMenuDisplay();
+        }
+        )
+      } else if (userChoice.mainMenuPrompt === `View all roles`) {
+        const sql = `Select `
+        console.table(`viewing roles`)
+        db.query('SELECT * from role', function (err, results) {
+          //NEED A JOIN FOR THIS?
+          console.table(results);
+          mainMenuDisplay();
+        }
+        )
+      } else if (userChoice.mainMenuPrompt === `View all employees`) {
+        console.table(`Viewing employees`);
+        db.query(`Select * from employee`, function (err, results) {
+          console.table(results);
+          mainMenuDisplay();
+        })
+      } else if (userChoice.mainMenuPrompt === `Add a department`) {
+        inquirer.prompt(departmentPrompts)
+          .then((departmentData) => {
+            const sql = `INSERT INTO department (name) VALUES (?)`
+            const params = departmentData.departmentName;
+
+            db.query(sql, params, function (err, result) {
+              mainMenuDisplay();
+            })
+
+          })
+      } else if (userChoice.mainMenuPrompt === `Add a role`) {
+        inquirer.prompt(rolePrompts)
+          .then((roleData) => {
+            const sql = `INSERT INTO role (title) VALUES (?)`
+            const params1 = roleData.name;
+            const params2 = roleData.salary;
+            db.query(sql, params1, function (err, result) {
+              mainMenuDisplay();
+            }
+            )
+          }
+          )
       }
-      )
-    } else if (userChoice.mainMenuPrompt === `View all roles`) {
-      const sql = `Select `
-      console.table(`viewing roles`)
-      db.query('SELECT * from role', function (err, results){
-        //NEED A JOIN FOR THIS?
-        console.table(results);
-        mainMenuDisplay();
-      }
-      )
-    } else if (userChoice.mainMenuPrompt === `View all employees`) {
-      console.table(`Viewing employees`);
-      db.query(`Select * from employee`, function (err, results) {
-        console.table(results);
-        mainMenuDisplay();
-      })
-    }
-  })
+    })
 }
 
 
@@ -70,22 +119,10 @@ const mainMenuDisplay = () => {
 //title, department
 //GET RID OF: role_id
 
-// SELECT role, employee
-// FROM employee
-// JOIN role ON employee.role_id = role.title 
-
-
-
 //start app
 const init = () => mainMenuDisplay();
 
 init();
-
-//testing
-// db.query('SELECT * FROM role', function (err, results)
-// {
-//   console.log(results);
-// })
 
 app.use((req, res) => {
   res.status(404).end();
