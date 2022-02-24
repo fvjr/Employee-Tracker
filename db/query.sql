c902c770efd4a1bb3b5784eb717dddd3e708d90d
Fernando Vasquez <vasquezjrf@gmail.com>
2022-02-24 17:57:27 -0500

    further developed query for displaying employees

--- db/query.sql
+++ db/query.sql
@@ -18,3 +18,27 @@
 
 -- WHEN I choose to view all employees
 -- THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+
+-- FROM role
+-- join department on role.department_id = department.id;
+
+-- SELECT employee.id AS Employee_ID, employee.first_name AS First_Name, employee.last_name AS Last_Name, role.title AS Role, role.salary AS Salary
+-- FROM employee
+-- join role on employee.role_id  = role.id;
+
+
+-- FROM role
+-- join department on role.department_id = department.id;
+
+SELECT employee.id AS Employee_ID, employee.first_name AS First_Name, employee.last_name AS Last_Name, role.title AS Role, role.salary AS Salary, department.name as Department
+
+
+-- FROM employee
+-- join role on employee.role_id  = role.id,
+-- FROM role_id
+-- join department on role.department_id = department.name;
+
+FROM role
+join employee on role.id = employee.role_id
+join department on role.department_id = department.id;
+
