a9e739bb5655fe17b2457c65188b226c8d59f83b
Fernando Vasquez <vasquezjrf@gmail.com>
2022-02-24 14:34:37 -0500

    developed queries for role display option

--- db/query.sql
+++ db/query.sql
@@ -1,3 +1,17 @@
-SELECT role.title as title, employee.role_id as id
-FROM employee
-JOIN role ON employee.role_id = role.title;
+-- SELECT employee.id AS Employee_id, employee.first_name AS employee_first_name, department.name AS Department_name
+-- FROM employee
+-- JOIN department
+-- ON employee.role_id = department.name
+
+-- SELECT role.id as Role_id, role.title as Role_title, role.salary AS salary, department.name as Department_name
+-- FROM role
+-- JOIN department on role.department_id = department.id;
+
+
+-- WHEN I choose to view all roles
+-- THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+
+SELECT role.title AS Role_Title, role.id AS Role_id, department.name AS Department, role.salary AS Salary 
+
+FROM role
+join department on role.department_id = department.id;
